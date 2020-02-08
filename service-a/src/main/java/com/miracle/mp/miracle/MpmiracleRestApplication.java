92c05ce05960b85595ea3deab503f28c64813cea
kencytbb <taikency@gmail.com>
2020-02-08 13:38:23 +0700

    upload file to repository

---
+++ service-a/src/main/java/com/miracle/mp/miracle/MpmiracleRestApplication.java
@@ -0,0 +1,11 @@
+package com.miracle.mp.miracle;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MpmiracleRestApplication extends Application {
+}
