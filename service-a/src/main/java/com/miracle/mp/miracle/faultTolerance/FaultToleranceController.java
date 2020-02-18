8e7535c0a555be8db030800a9be35c023f7e01cb
ntvi01 <nguyentruongvi2803@gmail.com>
2020-02-18 17:55:00 +0700

    ...

--- service-a/src/main/java/com/miracle/mp/miracle/faultTolerance/FaultToleranceController.java
+++ service-a/src/main/java/com/miracle/mp/miracle/faultTolerance/FaultToleranceController.java
@@ -1,30 +1,54 @@
 package com.miracle.mp.miracle.faultTolerance;
 
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
 @Path("/faultTolerance")
 public class FaultToleranceController {
 
     @GET
-    @Path("{id}")
-    public Response getPostById(@PathParam("id") Long id) throws InterruptedException {
+    @Path("/fallback/{id}")
+    public Response getNameById(@PathParam("id") Long id) throws InterruptedException {
         return Response.ok(getDataFromLongRunningTask(id)).build();
     }
 
-    public String getDefaultPost(Long id) {
-        return "fault" + id.toString();
+    @GET
+    @Path("/retry")
+    public Response callRetry() {
+        return Response.ok(accessFlakyService()).build();
     }
 
-    @Timeout(4000)
-    @Fallback(fallbackMethod = "getDefaultPost")
+    public String getDefaultName(Long id) {
+        return "John" + id.toString();
+    }
+
+    @Timeout(3)
+    @Fallback(fallbackMethod = "getDefaultName")
     public String getDataFromLongRunningTask(Long id) throws InterruptedException {
         Thread.sleep(4500);
         return "duke" + id.toString();
+    }
+
+    @Retry(maxDuration = 5000, maxRetries = 3, delay = 500, jitter = 200)
+    public String accessFlakyService() {
+    
+        System.out.println("Trying to access flaky service at " + LocalTime.now());
+    
+        if (ThreadLocalRandom.current().nextLong(1000) < 50) {
+            return "flaky duke";
+        } else {
+            throw new RuntimeException("Flaky service not accessible");
+        }
     }
 }
