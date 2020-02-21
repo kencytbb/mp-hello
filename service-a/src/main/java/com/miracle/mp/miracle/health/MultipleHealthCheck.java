2183ca1630d2d37cff611d20d2669f4499a8c3d6
ntvi01 <nguyentruongvi2803@gmail.com>
2020-02-21 16:16:20 +0700

    health check

--- service-a/src/main/java/com/miracle/mp/miracle/health/MultipleHealthCheck.java
+++ service-a/src/main/java/com/miracle/mp/miracle/health/MultipleHealthCheck.java
@@ -1,20 +1,21 @@
 package com.miracle.mp.miracle.health;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.Readiness;
 
 @Readiness
-@ApplicationScoped
-public class ServiceReadyHealthCheck implements HealthCheck {
+@Liveness
+public class MultipleHealthCheck implements HealthCheck {
 
     @Override
     public HealthCheckResponse call() {
-
         return HealthCheckResponse
-                .named("ServiceReadyHealthCheck")
+                .named("MultipleHealthCheck")
+                .withData("foo", "bar")
+                .withData("uptime", 42)
+                .withData("isReady", true)
                 .up()
                 .build();
     }
